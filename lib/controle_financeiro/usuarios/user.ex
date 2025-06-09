836a0655aae4baff0ab6f35088ba5df4f6e2ab8e
Ian Lucas S. César <129915834+Ian-LS-Cesar@users.noreply.github.com>
2025-06-09 01:45:27 -0300

    Correçao em Schema Users

--- lib/controle_financeiro/usuarios/user.ex
+++ lib/controle_financeiro/usuarios/user.ex
@@ -9,11 +9,25 @@ defmodule ControleFinanceiro.Usuarios.User do
     field :data_criacao, :utc_datetime
     field :data_atualizacao, :utc_datetime
 
-    has_many :transactions, ControleFinanceiro.Usuarios.User, foreign_key: :id_user
-    has_many :tags, ControleFinanceiro.Usuarios.User, foreign_key: :id_user
+    has_many :transactions, ControleFinanceiro.Transacoes.Transaction, foreign_key: :id_user
+    has_many :tags, ControleFinanceiro.Categorias.Tag, foreign_key: :id_user
     timestamps(type: :utc_datetime)
   end
 
+  @spec changeset(
+          {map(),
+           %{
+             optional(atom()) =>
+               atom()
+               | {:array | :assoc | :embed | :in | :map | :parameterized | :supertype | :try,
+                  any()}
+           }}
+          | %{
+              :__struct__ => atom() | %{:__changeset__ => any(), optional(any()) => any()},
+              optional(atom()) => any()
+            },
+          :invalid | %{optional(:__struct__) => none(), optional(atom() | binary()) => any()}
+        ) :: Ecto.Changeset.t()
   @doc false
   def changeset(user, attrs) do
 
