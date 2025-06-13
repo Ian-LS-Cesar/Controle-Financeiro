3aeb4b1b1b287afb993d19a56b7f5ea49f6f5319
letticca <leticia.mlcunha.816@gmail.com>
2025-06-13 18:37:00 -0300

    Adicionando front ao projeto, tela de login com react pronta mas faltam alguns detalhes

---
+++ front/src/components/Login/Login.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {FaUser, FaLock} from 'react-icons/fa';
+import { useState } from 'react';
+import "./Login.css";
+const Login = () => {
+  // a primeira le a segunda altera 
+  const[username,setUsername] = useState("");
+  const[password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("enviando dados:" + username + password);
+  }
+
+  return (
+    <div className='container'>  
+        <form onSubmit={handleSubmit}> 
+            <h1> Login</h1>
+            
+            <div className='input-field'> 
+              <input type="email" placeholder='Email'
+              required
+              onChange={(e) => setUsername(e.target.value)}/>
+              <FaUser className= "icon"/>
+            </div>
+
+            <div className='input-field'> 
+              <input type="password" placeholder='Senha'
+              onChange={(e) => setPassword = (e.target.value)}/>
+              <FaLock className= "icon"/>
+            </div>    
+
+            {/* <div className='recall-forget'>
+              <label>
+                  <input type="checkbox"/>
+                  lembre de mim
+              </label>
+              <a href="#">Esqueceu a senha?</a>
+            </div> */}
+
+            <button>Entrar</button>   
+
+        </form>
+    </div>
+  )
+}
+
+export default Login
